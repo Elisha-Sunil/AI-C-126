8bab94b001f33b92d027215de17dbc4ccaf72613
Elisha-Sunil <71624635+Elisha-Sunil@users.noreply.github.com>
2020-10-15 17:32:22 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 song="";
+scoreLeftWrist=0;
+
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -7,10 +14,44 @@ function setup(){
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, modeLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist="+scoreLeftWrist);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+ leftWristX +"leftWristY="+ leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX="+ rightWristX +"rightWristY="+ rightWristY);
+    }
+}
+function modeLoaded(){
+    console.log('Posenet Is Initialized');
+
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("#0000FF");
+    stroke("#0000FF");
+    if(scoreLeftWrist> 0.2){
+
+    
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimals=floor(InNumberleftWristY);
+    volume=remove_decimals/500;
+    document.getElementById("volume").innerHTML="Volume="+volume;
+    song.setVolume(volume);
+    }
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+
